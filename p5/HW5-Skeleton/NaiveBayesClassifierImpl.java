28cbd19bf0245b8a76aed429a36ae33329fdf1fe
agrimpandey <agrimpandey@gmail.com>
2017-05-03 14:43:16 -0500

    divide by zero exception

--- p5/HW5-Skeleton/NaiveBayesClassifierImpl.java
+++ p5/HW5-Skeleton/NaiveBayesClassifierImpl.java
@@ -1,3 +1,5 @@
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.HashSet;
@@ -29,6 +31,11 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	//full vocabulary, update in training, cardinality is necessary for smoothing
 	private HashSet<String> vocabulary = new HashSet<String>();
 
+	private ArrayList<Instance> instances = new ArrayList<Instance>();
+
+	private int words_c = 0;
+	private int words_t = 0;
+	private int words_h = 0;
 	/**
 	 * Trains the classifier with the provided training data
    Should iterate through the training instances, and, for each word in the documents, update the variables above appropriately.
@@ -37,8 +44,12 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	@Override
 	public void train(Instance[] trainingData) {
 		// TODO : Implement
+
+
 		for(int i = 0; i< trainingData.length; i++)
 		{
+			instances.add(trainingData[i]);
+
 			if (trainingData[i].label == Label.COMEDY){
 				genreCounts(trainingData[i], comedyCounts);
 			}
@@ -49,16 +60,21 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 				genreCounts(trainingData[i], tragedyCounts);
 			}
 		}
+
+		comedyPrior = p_l(Label.COMEDY);
+		tragedyPrior = p_l(Label.TRAGEDY);
+		historyPrior = p_l(Label.HISTORY);
+		
 	}
 
 	private void genreCounts(Instance tempInst, HashMap<String, Integer> gCounts){
 		for(String token: tempInst.words)
 		{
+			vocabulary.add(token);
 			if(gCounts.containsKey(token))
 			{
 				int v = gCounts.get(token);
-				v++;
-				//gCounts.+=1;
+				gCounts.put(token,++v);
 			}else{
 				gCounts.put(token,1);
 			}
@@ -71,6 +87,18 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	 */
 	public void documents_per_label_count(){
 		// TODO : Implement
+		int t_count = 0;		
+		int c_count = 0;
+		int h_count = 0;
+		for(Instance instance: instances){
+			if(instance.label==Label.COMEDY)
+				c_count++;
+			else if(instance.label==Label.HISTORY)
+				h_count++;
+			else if(instance.label==Label.TRAGEDY)
+				t_count++;
+		}
+		System.out.println(c_count + "  "  + t_count + "  " + h_count);
 	}
 
 	/*
@@ -79,7 +107,23 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	 */
 	public void words_per_label_count(){
 		// TODO : Implement
-		//System.out.println(x);
+
+		for(String word: tragedyCounts.keySet())
+		{
+			words_t += tragedyCounts.get(word);
+		}
+		for(String word: comedyCounts.keySet())
+		{
+			words_c += comedyCounts.get(word);
+		}
+		for(String word: historyCounts.keySet())
+		{
+			words_h += historyCounts.get(word);
+		}
+		System.out.println("" + words_c);
+		System.out.println("" + words_t);
+		System.out.println("" + words_h);
+
 	}
 
 	/**
@@ -88,12 +132,15 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	@Override
 	public double p_l(Label label) {
 		// TODO : Implement
-		if(label==Label.COMEDY)
-			return comedyPrior;
-		else if(label==Label.TRAGEDY)
-			return tragedyPrior;
-		else if(label==Label.HISTORY)
-			return historyPrior;
+		if(label==Label.COMEDY){
+			return words_c/(words_c + words_t + words_h);
+		}
+		else if(label==Label.TRAGEDY){
+			return words_t/(words_c + words_t + words_h);
+		}
+		else if(label==Label.HISTORY){
+			return words_h/(words_c + words_t + words_h);
+		}
 		return 0;
 	}
 
@@ -106,36 +153,31 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 		// TODO : Implement
 		double delta = 0.00001;
 		double numerator = delta;
-		double sum_v = 0.00;
+		double denominator = vocabulary.size()*delta;
 
 		if(label==Label.COMEDY)
 		{
-			numerator += comedyCounts.get(word);
-			sum_v = calc_sum_v(comedyCounts);
+			if(comedyCounts.containsKey(word)){
+				numerator += comedyCounts.get(word);
+			}
+			denominator += words_c;
 		}
 		else if(label==Label.TRAGEDY)
 		{
-			numerator += tragedyCounts.get(word);
-			sum_v = calc_sum_v(tragedyCounts);
-
+			if(tragedyCounts.containsKey(word)){
+				numerator += tragedyCounts.get(word);
+			}
+			denominator += words_t;
 		}			
 		else if(label==Label.HISTORY)
 		{
-			numerator += historyCounts.get(word);
-			sum_v = calc_sum_v(historyCounts);
+			if(historyCounts.containsKey(word)){
+				numerator += historyCounts.get(word);
+			}
+			denominator += words_h;
 		}
-
-		double denominator = vocabulary.size()*delta + sum_v;
 
 		return numerator/denominator;
-	}
-
-	private double calc_sum_v(HashMap<String, Integer> map){
-		double total = 0.00;
-		for(Integer val: map.values()){
-			total += val;
-		}
-		return total;
 	}
 
 	/**
@@ -146,34 +188,69 @@ public class NaiveBayesClassifierImpl implements NaiveBayesClassifier {
 	public Label classify(Instance ins) {
 
 		// TODO : Implement
-		double prob_h = 1;
-		double prob_t = 1;
-		double prob_c = 1;
-
-		for(String w: ins.words)
-		{
-			prob_h *= Math.log(p_w_given_l(w, Label.COMEDY));
-			prob_t *= Math.log(p_w_given_l(w, Label.TRAGEDY));
-			prob_c *= Math.log(p_w_given_l(w, Label.HISTORY));
-		}
-
-		prob_h += Math.log(historyPrior);
-		prob_t += Math.log(tragedyPrior);
-		prob_c += Math.log(comedyPrior);
-
-		double maxprob = Math.max(Math.max(prob_h, prob_c), prob_t);
-		if(maxprob == prob_h)
-			return Label.HISTORY;
-		else if(maxprob == prob_c)
-			return Label.COMEDY;
-		else if(maxprob == prob_t)
-			return Label.TRAGEDY;
 		//Initialize sum probabilities for each label
 		//For each word w in document ins
 		//compute the log (base e or default java log) probability of w|label for all labels (COMEDY, TRAGEDY, HISTORY)
 		//add to appropriate sum
 		//Return the Label of the maximal sum probability
+		
+		double prob_h = Math.log(historyPrior);
+		double prob_t = Math.log(tragedyPrior);
+		double prob_c = Math.log(comedyPrior);
 
+		for(String w: ins.words)
+		{
+			prob_h += Math.log(p_w_given_l(w, Label.COMEDY));
+			prob_t += Math.log(p_w_given_l(w, Label.TRAGEDY));
+			prob_c += Math.log(p_w_given_l(w, Label.HISTORY));
+		}
+
+		double maxprob = Math.max(Math.max(prob_h, prob_c), prob_t);
+		
+		// breaking ties
+		if(maxprob == prob_h && maxprob == prob_c && maxprob == prob_t)
+		{
+			double max_priorprob = Math.max(Math.max(comedyPrior,historyPrior), 
+					tragedyPrior);
+			//no need to break ties here since test cases do not have equal 
+			// prior probabilities
+			if(max_priorprob == historyPrior)
+				return Label.HISTORY;
+			else if(max_priorprob == comedyPrior)
+				return Label.COMEDY;
+			else if(max_priorprob == tragedyPrior)
+				return Label.TRAGEDY;
+		}
+		else if(maxprob == prob_h && maxprob == prob_c)
+		{
+			if(comedyPrior >= historyPrior)
+				return Label.COMEDY;
+			else
+				return Label.HISTORY;
+
+		}
+		else if(maxprob == prob_h && maxprob == prob_t)
+		{
+			if(historyPrior >= tragedyPrior)
+				return Label.HISTORY;
+			else
+				return Label.TRAGEDY;
+		}
+		else if(maxprob == prob_c && maxprob == prob_t)
+		{
+			if(comedyPrior >= tragedyPrior)
+				return Label.COMEDY;
+			else
+				return Label.TRAGEDY;
+		}
+		// order matters
+		else if(maxprob == prob_h)
+			return Label.HISTORY;
+		else if(maxprob == prob_c)
+			return Label.COMEDY;
+		else if(maxprob == prob_t)
+			return Label.TRAGEDY;
+		
 		return null; 
 	}
 
